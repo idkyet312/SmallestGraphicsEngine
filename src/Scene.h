d12ac844d22c9a14458aa22608add1f7b0aea57e
idkyet312 <160145816+idkyet312@users.noreply.github.com>
2026-07-18 15:06:17 +1000

    Enable MSAA by default

--- src/Scene.h
+++ src/Scene.h
@@ -175,8 +175,8 @@ struct Scene {
     // NVIDIA Blast + Box3D destructible house
     bool  useDestruction = true;
     bool  showHelicopter = true;   // draw + simulate the hovering attack heli
-    bool  enableMSAA = false;
-    bool  enableFXAA = true;
+    bool  enableMSAA = true;
+    bool  enableFXAA = false;
     int   destructionGridX = 4;
     int   destructionGridY = 3;
     int   destructionGridZ = 4;
